50845ba5c9c4132a9c27d4ef69bb9ca1ae4c8fa5
oliviac12 <oliviachen56@gmail.com>
2015-10-06 12:38:57 -0400

    added comment

--- Raw Code/Cali production exploration.py
+++ Raw Code/Cali production exploration.py
@@ -13,19 +13,20 @@ import geocoder as geo
 import pandas as pd
 data = pd.read_csv('MeasuredProduction1.csv', header = False)
 
-
+# Subset the data to specific columns we need
 Cali_cleanData = data[['Application Number','Program','Host Customer Physical Address City',
 'Host Customer Physical Address Zip Code','Production Period End Date','Period kWh Production']]
-
+# rename variable zipcode
 Cali_cleanData.rename(columns={'Host Customer Physical Address Zip Code': 'ZipCode'}, inplace=True)
-
+#open the zipcode data set to merge with the cali production data set
 zips = pd.read_csv('zipcode.csv', header = False)
 Zip = zips[['zip code', 'latitude', 'longitude']]
 Zip.columns = ['ZipCode', 'Latitude', 'Longtitude']
-
-   
+#convert zipcode as string in order for mergeing later 
 Cali_cleanData['ZipCode'] = Cali_cleanData['ZipCode'].astype(str)
+#merge the zipcode and cali_prodcution
 Merge1 = pd.merge(Cali_cleanData, Zip, on='ZipCode', how='inner')
+#clean the merged dataset, fixed gps location format
 latitude = []
 for row in Merge1['Latitude']:
     latitude.append(row.replace('"', '').strip())
@@ -34,13 +35,14 @@ longtitude = []
 for row in Merge1['Longtitude']:
     longtitude.append(row.replace('"', '').strip())
 Merge1['longtitude'] = longtitude
+#finalize the merge dataset
 Merge1 = Merge1.drop('Latitude', 1)
 Merge1 = Merge1.drop('Longtitude', 1)
-Merge1['latitude'] = Merge1['latitude'].astype(float)
-Merge1['longtitude'] = Merge1['longtitude'].astype(float)
 
 
-#how to use elevation how to use elevation ???????
+
+#how to use elevation how to use elevation ???????  not sure about everything below this line
+######################################################################################
 elevation = []
 gps = zip(latitude, longtitude)
 unique = set(gps)
--- Raw Code/Weatherapi_firstattempt.py
+++ Raw Code/Weatherapi_firstattempt.py
@@ -6,6 +6,7 @@ Created on Fri Sep 18 12:18:11 2015
 """
 import urllib2
 import json
+import requests
 f = urllib2.urlopen('http://api.wunderground.com/api/e6a109eddf157c39/planner_07010731/q/CA/San_Francisco.json')
 json_string = f.read()
 parsed_json = json.loads(json_string)
@@ -16,3 +17,19 @@ e_Date = parsed_json['trip']['period_of_record']['date_end']['date']['day']
 e_Monoth = parsed_json['trip']['period_of_record']['date_end']['date']['month']
 e_year = Date = parsed_json['trip']['period_of_record']['date_end']['date']['year']
 print "Current temperature in %s is: %s" % (location, temp_f)
+
+url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCNDMS&locationid=ZIP:94404&startdate=2007-01-01&enddate=2007-12-30'
+
+url1 = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories?'
+url2 = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes'
+URL3 = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datasets'
+response = requests.get(url, headers={'token':'FDTLhHxYJAIGtXwiIvSSmpmyipjiMEHw'})
+parsed_json = response.json()
+datatypeid=MNTM
+
+URL1 = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCNDMS&locationid=CNTY:San Diego&startdate=2010-05-01&enddate=2010-06-01'
+response = requests.get(URL1, headers= {'token':'FDTLhHxYJAIGtXwiIvSSmpmyipjiMEHw'})
+response = requests.get(url2, headers= {'token':'FDTLhHxYJAIGtXwiIvSSmpmyipjiMEHw'})
+
+location = "http://www.ncdc.noaa.gov/cdo-web/api/v2/locationcategories"
+response = requests.get(location, headers= {'token':'FDTLhHxYJAIGtXwiIvSSmpmyipjiMEHw'})
