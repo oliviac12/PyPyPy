ad79c36407e67d207b5dc88af5816267a6f3070d
Olivia Chen <oliviachen56@gmail.com>
2016-02-04 19:18:52 -0500

    calculate points

--- Raw_Code/solaranywhere.py
+++ Raw_Code/solaranywhere.py
@@ -2,17 +2,17 @@ import numpy as py
 import itertools
 
 #finding all tge points covering california based on 10km tiles
-log1 = list(py.arange(-124.15, -120, 0.1))
-log2 = list( py.arange(-123.75, -114.7, 0.1))
-log = log1 + log2
-log= [round(ele, 2) for ele in log]
-lat1 = list(py.arange(39.05, 42, 0.1))
-lat2 = list(py.arange(32.55, 39.05, 0.1))
-lat = lat1 + lat2
-lat = [round(ele, 2) for ele in lat]
+log1 = list(py.arange(-124.15, -114.7, 0.1))
+#log2 = list( py.arange(-123.75, -114.7, 0.1))
+#log = log1 + log2
+log1= [round(ele, 2) for ele in log1]
+lat1 = list(py.arange(32.55, 42, 0.1))
+#lat2 = list(py.arange(32.55, 39.05, 0.1))
+#lat = lat1 + lat2
+lat1 = [round(ele, 2) for ele in lat1]
 Points = []
-for Ele in lat:
-    for ele in log:
+for Ele in lat1:
+    for ele in log1:
         point = (Ele, ele)
         Points.append(point)
         
